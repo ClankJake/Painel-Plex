7030fe5f51a5fb3e1c352d9c2ddf6d23158d42f0
ClankJake <44306273+ClankJake@users.noreply.github.com>
2025-08-30 15:49:47 -0300

    Update __init__.py

--- app/services/tautulli/__init__.py
+++ app/services/tautulli/__init__.py
@@ -1,11 +1,10 @@
 # app/services/tautulli/__init__.py
 
 from .api_client import TautulliApiClient
-from .notifier_handler import NotifierHandler
 from .stats_handler import StatsHandler
 
 __all__ = [
     "TautulliApiClient",
-    "NotifierHandler",
     "StatsHandler",
 ]
+
