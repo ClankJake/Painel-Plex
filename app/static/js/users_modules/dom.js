6be48c4840b199f7f1fa9d04d16336fe50a2614f
ClankJake <44306273+ClankJake@users.noreply.github.com>
2025-10-01 17:23:48 -0300

    Update dom.js

--- app/static/js/users_modules/dom.js
+++ app/static/js/users_modules/dom.js
@@ -8,7 +8,6 @@
  */
 
 export const userGrid = document.getElementById('userGrid');
-export const userGridLoader = document.getElementById('user-grid-loader'); // NOVO: Loader para scroll infinito
 export const inviteListDiv = document.getElementById('inviteList');
 export const createInviteButton = document.getElementById('createInviteButton');
 export const refreshButton = document.getElementById('refreshButton');
